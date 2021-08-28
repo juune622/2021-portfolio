9dcff3bc73f235b626b147ae41cd354e245c7c4d
juune622 <juune622@naver.com>
2021-08-28 15:48:23 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -6,23 +6,23 @@ $(".popup-wrapper .home").click(function(){
 })
 $(".more").click(function(){
     $(".sidebar").css("left","50%")
-    $(".bgCover").show("slow")
+    $(".bgCover").show()
     $(".close").css("top","0")
 })
 $(".close").click(function(){
     $(".sidebar").css("left","100%")
-    $(".bgCover").hide("slow")
+    $(".bgCover").hide()
     $(".close").css("top","-100px")
 })
 $(".popup .popup-wrapper ul li").eq(0).siblings().click(function(){
     $(".sidebar").css("left","50%")
-    $(".bgCover").show("slow")
+    $(".bgCover").show()
     $(".close").css("top","0")
     $(".popup").removeClass("open")
 })
 $(".contents .main .btn-wrapper a.portfolio").click(function(){
     $(".sidebar").css("left","50%")
-    $(".bgCover").show("slow")
+    $(".bgCover").show()
     $(".close").css("top","0")
 })
 
